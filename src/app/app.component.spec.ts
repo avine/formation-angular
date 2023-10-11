bf58f8f90a18bb1ba6b803793f83109a2c4eae3b
Stéphane Francel <stephane.francel@gmail.com>
2023-10-11 10:15:40 +0200

    Lab 3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,49 @@
-import { TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { Product } from './product/product.types';
 
 describe('AppComponent', () => {
-  beforeEach(() =>
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
-    }),
-  );
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    });
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'my first component'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('my first component');
+  it('should display the products', () => {
+    const productDebugElements = fixture.debugElement.queryAll(By.css('app-product'));
+    productDebugElements.forEach((productDebugElement, index) => {
+      expect(productDebugElement.properties['product']).toBe(component.products[index]);
+    });
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should update the total when "addToBasket" class method is called', () => {
+    const productDebugElements = fixture.debugElement.queryAll(By.css('app-product'));
+    productDebugElements[0].triggerEventHandler('addToBasket', productDebugElements[0].properties['product']);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome to my first component!');
+
+    expect((fixture.nativeElement as HTMLElement).querySelector('header p')?.textContent).toContain(
+      `Votre panier s'élève à ${component.products[0].price} €`,
+    );
+  });
+
+  it('should update the total when a product emits the "addToBasket" event', () => {
+    component.addToBasket({ price: 1 } as Product);
+    expect(component.total).toBe(1);
+
+    component.addToBasket({ price: 2 } as Product);
+    expect(component.total).toBe(3);
   });
 });
--- src/app/product/product.component.spec.ts
+++ src/app/product/product.component.spec.ts
@@ -12,10 +12,37 @@ describe('ProductComponent', () => {
     });
     fixture = TestBed.createComponent(ProductComponent);
     component = fixture.componentInstance;
+    component.product = { id: 'ID', title: 'TITLE', description: 'DESC', photo: 'PHOTO', price: 1, stock: 2 };
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the product photo as image url', () => {
+    expect((fixture.nativeElement as HTMLElement).querySelector<HTMLImageElement>('.card-img-top')?.src).toContain(
+      'PHOTO',
+    );
+  });
+
+  it('should display the product description', () => {
+    expect((fixture.nativeElement as HTMLElement).querySelector('.card-header small')?.textContent).toContain('DESC');
+  });
+
+  it('should display the product title', () => {
+    expect((fixture.nativeElement as HTMLElement).querySelector('.card-link')?.textContent).toContain('TITLE');
+  });
+
+  it('should display the product price', () => {
+    expect((fixture.nativeElement as HTMLElement).querySelector('.card-text')?.textContent).toContain('1');
+  });
+
+  it('should emit addToBasket event with the given product when the button is clicked', () => {
+    const addToBasketSpy = spyOn(component.addToBasket, 'emit');
+
+    (fixture.nativeElement as HTMLElement).querySelector('button')?.click();
+
+    expect(addToBasketSpy).toHaveBeenCalledWith(component.product);
   });
 });
